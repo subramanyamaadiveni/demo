efc958d90ac4de6f1710aa9b253ad026a612abcb
subramanyamaadiveni <114573043+Subbu0068@users.noreply.github.com>
2024-11-18 15:00:52 +0530

    my first code commit and 5 th normal commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello BridgeLabs");
+        System.out.println(add(2,3));
+    }
+    static int add(int a,int b){
+        int c= a+b;
+        return c;
     }
 }
